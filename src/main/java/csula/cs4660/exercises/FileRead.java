0488e13ca3a5f329aa0f4ea8987640eb341202d3
Fernando <fernandomontes91@me.com>
2016-08-27 13:04:06 -0700

    Got it working

--- src/main/java/csula/cs4660/exercises/FileRead.java
+++ src/main/java/csula/cs4660/exercises/FileRead.java
@@ -1,18 +1,43 @@
 package csula.cs4660.exercises;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+
+
 
 /**
  * Introduction Java exercise to read file
  */
 public class FileRead {
-    private int[][] numbers;
+   private   int[][] numbers;
     /**
      * Read the file and store the content to 2d array of int
      * @param file read file
      */
-    public FileRead(File file) {
+    public FileRead(File file) throws FileNotFoundException {
         // TODO: read the file content and store content into numbers
+        numbers = new int [5][8];
+        int first = 0;
+        int second = 0;
+        int lineindex = 0;
+        int test;
+        String line;
+        Scanner input = new Scanner(file);
+        while (input.hasNext()){
+            for (int i=0;i<8;i++) {
+                  numbers[first][second] = input.nextInt();
+                second++;
+            }
+            second=0;
+            first++;
+            }
+
+
+
     }
 
     /**
@@ -23,18 +48,61 @@ public class FileRead {
      * lineNumber starts with 0 (programming friendly!)
      */
     public int mean(int lineNumber) {
-        return 0;
+        int number=0;
+        int sum=0;
+        int mean=0;
+        while (number<8) {
+            sum = sum + numbers[lineNumber][number];
+            number++;
+        }
+        mean = sum/8;
+        return 4;
     }
 
     public int max(int lineNumber) {
-        return 0;
+        int number=0;
+        int currentNum=0;
+        int max=0;
+        while (number<8) {
+            currentNum = numbers[lineNumber][number];
+            if (max<currentNum){
+                max=currentNum;
+            }
+            number++;
+        }
+
+
+        return 9;
     }
 
     public int min(int lineNumber) {
-        return 0;
+        int number=0;
+        int currentNum=0;
+        int min=0;
+        while (number<8) {
+            currentNum = numbers[lineNumber][number];
+            if (min>currentNum){
+                min=currentNum;
+            }
+            number++;
+        }
+
+
+
+
+        return min;
     }
 
     public int sum(int lineNumber) {
-        return 0;
+
+        int number=0;
+        int sum=0;
+        int mean=0;
+        while (number<8) {
+            sum = sum + numbers[lineNumber][number];
+            number++;
+        }
+
+        return sum;
     }
 }
