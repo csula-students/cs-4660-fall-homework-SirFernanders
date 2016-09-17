5e4cf96a0dc9f6b48c83aa301584cbc26d5601a1
Fernando <fernandomontes91@me.com>
2016-09-17 10:54:48 -0700

    Changes

--- src/main/java/csula/cs4660/graphs/representations/AdjacencyList.java
+++ src/main/java/csula/cs4660/graphs/representations/AdjacencyList.java
@@ -1,13 +1,15 @@
 package csula.cs4660.graphs.representations;
 
+
+
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Node;
 
 import java.io.File;
-import java.util.Collection;
+
+import java.io.FileNotFoundException;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * Adjacency list is probably the most common implementation to store the unknown
@@ -18,7 +20,64 @@ import java.util.Optional;
 public class AdjacencyList implements Representation {
     private Map<Node, Collection<Edge>> adjacencyList;
 
-    public AdjacencyList(File file) {
+    public AdjacencyList(File file)  {
+
+        adjacencyList = new HashMap<>();
+
+        int numberOfNodes;
+        int placeHolder;
+        Node fromNode;
+        Node toNode;
+        int edgeValue;
+        Edge edge;
+        String nextLine;
+        String[] split;
+        Node tempNode;
+
+
+
+
+
+        //scanner to read the file. Try & Caught to avoid a exception.
+        try {
+            Scanner s  = new Scanner(file);
+            numberOfNodes = Integer.parseInt(s.nextLine());
+
+
+            //Pulls how many nodes are in the graph then initiates the map with that many.
+            for (int g=0; g<numberOfNodes;g++){
+                tempNode = new Node(g);
+                //adds nodes/keys
+                addNode(tempNode);
+
+            }
+
+
+            //Gets the following lines and splits them into an array then puts the information in corresponding locations
+            //then calls the add edge function to add the edge to the graph
+            while (s.hasNextLine()) {
+                    nextLine = s.nextLine();
+                    split = nextLine.split(":");
+                    placeHolder = Integer.parseInt(split[0]);
+                    fromNode = new Node<>(placeHolder);
+                    placeHolder = Integer.parseInt(split[1]);
+                    toNode = new Node<>(placeHolder);
+                    edgeValue = Integer.parseInt(split[2]);
+                    edge = new Edge(fromNode,toNode,edgeValue);
+
+
+
+
+                    //add edge/values
+                    addEdge(edge);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
     public AdjacencyList() {
@@ -27,36 +86,139 @@ public class AdjacencyList implements Representation {
 
     @Override
     public boolean adjacent(Node x, Node y) {
+        if (adjacencyList.get(x).toString().contains(y.toString())){
+            return true;
+        }
+        else if(adjacencyList.get(y).toString().contains(x.toString())){
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public List<Node> neighbors(Node x) {
-        return null;
+
+        List<Node> temp = new LinkedList<>() ;
+        for (int c=0; c<adjacencyList.size();c++){
+
+            //This if statement is to avoid calling itself as a neighbor.
+            if(new Node(c).toString().equals(x.toString())){
+                c++;
+            }
+            if(adjacencyList.get(x).toString().contains(new Node(c).toString())){
+                temp.add(new Node(c));
+
+            }
+
+        }
+
+        return temp;
+
     }
 
     @Override
     public boolean addNode(Node x) {
-        return false;
+        //add key
+
+        if (!adjacencyList.containsKey(x)) {
+            adjacencyList.put(x, new ArrayList<>());
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public boolean removeNode(Node x) {
-        return false;
+        //looks through every edge in the collection looking for calls to the node being removed
+        //then removes any edge going to the node being removed.
+        boolean g=false;
+        List<Edge> stuff= new ArrayList<>();
+        if(adjacencyList.containsKey(x))g=true;
+        if(adjacencyList.containsKey(x)) {
+            adjacencyList.remove(x);
+            adjacencyList.forEach((node, edges) -> {
+                adjacencyList.get(node).forEach(edge -> {
+                    if (edge.getTo().equals(x)) {
+                        stuff.add(edge);
+                    }
+                });
+            });
+
+        }
+        stuff.forEach(edge -> {
+            removeEdge(edge);
+        });
+
+        return g;
     }
 
     @Override
     public boolean addEdge(Edge x) {
-        return false;
+        Edge temp;
+        boolean i= false;
+
+        Iterator<Edge> edges = adjacencyList.get(x.getFrom()).iterator();
+
+        while(edges.hasNext()) {
+            temp = edges.next();
+            if(temp.getTo().equals(x.getTo())){
+                if(temp.getFrom().equals(x.getFrom())) {
+                    i = false;
+                    break;
+                }
+            }
+            else{
+
+                i = true;
+            }
+
+        }
+
+        if (!edges.hasNext()||i==true){
+            adjacencyList.get(x.getFrom()).add(x);
+            i=true;
+        }
+
+        return i;
     }
 
     @Override
     public boolean removeEdge(Edge x) {
-        return false;
+        Edge temp;
+        boolean i= false;
+
+        Iterator<Edge> edges = adjacencyList.get(x.getFrom()).iterator();
+
+        while(edges.hasNext()) {
+            temp = edges.next();
+            if(temp.getTo().equals(x.getTo())){
+                if(temp.getFrom().equals(x.getFrom())) {
+                    i = true;
+                    edges.remove();
+                    break;
+                }
+            }
+            else{
+                i = false;
+            }
+
+        }
+
+
+
+        return i;
     }
+
 
     @Override
     public int distance(Node from, Node to) {
+
+        //get linked list then pull distance for the second node
+        //need to figure out how to target different edges
+        adjacencyList.get(from);
         return 0;
     }
 
--- src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
+++ src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
@@ -4,8 +4,8 @@ import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Node;
 
 import java.io.File;
-import java.util.List;
-import java.util.Optional;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * Adjacency matrix in a sense store the nodes in two dimensional array
@@ -17,49 +17,148 @@ public class AdjacencyMatrix implements Representation {
     private int[][] adjacencyMatrix;
 
     public AdjacencyMatrix(File file) {
+
+        int numberOfNodes;
+        String nextLine;
+        Node tempNode;
+        String[] split;
+        int placeHolder;
+        int fromNode;
+        int toNode;
+        int edgeValue;
+
+
+        try {
+            Scanner s = new Scanner(file);
+            nextLine = s.nextLine();
+            numberOfNodes = Integer.parseInt(nextLine);
+
+            //Declares a Node Array with length for initial nodes.
+            nodes = new Node[numberOfNodes];
+
+            //Declares a  2D Array with lentgh on both sides of initial nodes.
+            adjacencyMatrix = new int[numberOfNodes][numberOfNodes];
+
+            //adds nodes to array locally to avoid making a new array every time I need to add a node.
+            for (int n = 0; n < numberOfNodes; n++) {
+                tempNode = new Node(n);
+                nodes[n] = tempNode;
+            }
+
+            //adds edges to 2D arry locally to avoid making a couple array every time I add a Edge.
+            while (s.hasNextLine()) {
+                nextLine = s.nextLine();
+                split = nextLine.split(":");
+                placeHolder = Integer.parseInt(split[0]);
+                fromNode = placeHolder;
+                placeHolder = Integer.parseInt(split[1]);
+                toNode = placeHolder;
+                edgeValue = Integer.parseInt(split[2]);
+                adjacencyMatrix[fromNode][toNode] = edgeValue;
+
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public AdjacencyMatrix() {
-
     }
 
     @Override
     public boolean adjacent(Node x, Node y) {
-        return false;
+        if(nodes[parseData(x)]==null||nodes[parseData(y)]==null) return false;
+        if(parseData(x)>nodes.length||parseData(y)>nodes.length||adjacencyMatrix[parseData(x)][parseData(y)]==0) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public List<Node> neighbors(Node x) {
-        return null;
+        List<Node> friends = new LinkedList<>();
+
+        for(int i=0; i < adjacencyMatrix.length; i++){
+            if(adjacent(x,new Node(i))){
+                friends.add(new Node(i));
+            }
+        }
+
+        return friends;
     }
 
     @Override
     public boolean addNode(Node x) {
+        if (nodes.length < parseData(x) + 1) {
+            Node[] tempArray = nodes;
+            nodes = new Node[parseData(x) + 1];
+            System.arraycopy(tempArray, 0, nodes, 0, tempArray.length);
+            nodes[Integer.parseInt(x.getData().toString())] = x;
+            int[][] tempMatrix = adjacencyMatrix;
+            adjacencyMatrix = new int[parseData(x)+1][parseData(x)+1];
+            System.arraycopy(tempMatrix,0,adjacencyMatrix,0,tempMatrix.length);
+
+
+            return true;
+        }
         return false;
+
     }
 
     @Override
     public boolean removeNode(Node x) {
-        return false;
+
+        if(nodes.length<parseData(x)||nodes[parseData(x)]==null){
+            return false;
+        }
+
+        nodes[parseData(x)]=null;
+        for(int i=0;i<adjacencyMatrix.length;i++) {
+            adjacencyMatrix[i][parseData(x)]=0;
+        }
+        return true;
     }
 
     @Override
     public boolean addEdge(Edge x) {
-        return false;
+        if(parseData(x.getFrom())+1>nodes.length||parseData(x.getTo())+1>nodes.length||adjacencyMatrix[parseData(x.getFrom())][parseData(x.getTo())]!=0) {
+            return false;
+        }
+        else{
+            adjacencyMatrix[parseData(x.getFrom())][parseData(x.getTo())]=x.getValue();
+            return true;
+        }
     }
 
     @Override
     public boolean removeEdge(Edge x) {
-        return false;
+        if(nodes.length<parseData(x.getFrom())||nodes.length<parseData(x.getTo())||adjacencyMatrix[parseData(x.getFrom())][parseData(x.getTo())]==0) {
+            return false;
+        }
+        adjacencyMatrix[parseData(x.getFrom())][parseData(x.getTo())]=0;
+        return true;
     }
 
     @Override
     public int distance(Node from, Node to) {
-        return 0;
+        if(nodes.length<parseData(from)||nodes.length<parseData(to)||nodes[parseData(from)]==null||nodes[parseData(to)]==null) {
+            return 0;
+        }
+        else{
+           return adjacencyMatrix[parseData(from)][parseData(to)];
+        }
     }
 
     @Override
     public Optional<Node> getNode(int index) {
         return null;
     }
+
+    private int parseData (Node x){
+        return Integer.parseInt(x.getData().toString());
+    }
+
 }
